b3713f7eef963889e71a1c23519a2628625f03eb
Sergey <alexroverandom@gmail.com>
2015-06-08 19:52:52 +0300

    no working lexer parser

---
+++ OSRV/OSRV/Compiler/Token.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSRV.Compiler
+{
+    public class Token
+    {
+        public Token(string type, string value, TokenPosition position)
+        {
+            Type = type;
+            Value = value;
+            Position = position;
+        }
+
+        public TokenPosition Position { get; set; }
+        public string Type { get; set; }
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Token: {{ Type: \"{0}\", Value: \"{1}\", Position: {{ Index: \"{2}\", Line: \"{3}\", Column: \"{4}\" }} }}", Type, Value, Position.Index, Position.Line, Position.Column);
+        }
+    }
+}
